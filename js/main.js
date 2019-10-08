552f5aa0c07f97a3fc0883a1ab672c7689677c48
Ba2ev <Ba2evd@gmail.com>
2019-10-08 18:52:16 +0300

    Отделяет создание карточки объявлений в card.js

--- js/main.js
+++ js/main.js
@@ -158,47 +158,6 @@ var createAdHTML = function (ad) {
   return adElement;
 };
 
-var createAdFeatureHTML = function (adFeature) {
-  var adFeatureElement = document.createElement('li');
-  adFeatureElement.className = 'popup__feature popup__feature--' + adFeature;
-  return adFeatureElement;
-};
-
-var createAdPhotoHTML = function (adPhoto) {
-  var adPhotoElement = document.createElement('img');
-  adPhotoElement.src = adPhoto;
-  adPhotoElement.className = 'popup__photo';
-  adPhotoElement.width = window.params.ad.adPhotoWidth;
-  adPhotoElement.height = window.params.ad.adPhotoHeight;
-  adPhotoElement.alt = 'Фотография жилья';
-  return adPhotoElement;
-};
-
-/**
- * Создаёт карточку с подробными параметрами предалагаемого жилья
- * @param {Ad} ad - элемент с заданным набором параметров
- * @return {HTMLElement} - элемент с заданным набором параметров
- */
-var createCardHTML = function (ad) {
-  var adFeaturesFragment = window.util.createFragment(ad.offer.features, createAdFeatureHTML);
-  var adPhotoesFragment = window.util.createFragment(ad.offer.photos, createAdPhotoHTML);
-
-  mapCardTemplate.querySelector('.popup__avatar').src = ad.author.avatar;
-  mapCardTemplate.querySelector('.popup__title').textContent = ad.offer.title;
-  mapCardTemplate.querySelector('.popup__text--address').textContent = ad.offer.address;
-  mapCardTemplate.querySelector('.popup__text--price').innerHTML = ad.offer.price + '&#x20bd;<span>/ночь</span>';
-  mapCardTemplate.querySelector('.popup__type').textContent = window.data.typesRU[window.data.typesEN.indexOf(ad.offer.type)];
-  mapCardTemplate.querySelector('.popup__text--capacity').textContent = ad.offer.rooms + ' комнаты для ' + ad.offer.guests + ' гостей';
-  mapCardTemplate.querySelector('.popup__text--time').textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
-  mapCardTemplate.querySelector('.popup__features').innerHTML = '';
-  mapCardTemplate.querySelector('.popup__features').appendChild(adFeaturesFragment);
-  mapCardTemplate.querySelector('.popup__description').textContent = ad.offer.description;
-  mapCardTemplate.querySelector('.popup__photos').innerHTML = '';
-  mapCardTemplate.querySelector('.popup__photos').appendChild(adPhotoesFragment);
-
-  return mapCardTemplate;
-};
-
 var renderPins = function () {
   var fragment = window.util.createFragment(ads, createAdHTML);
   mapPins.appendChild(fragment);
@@ -305,7 +264,7 @@ var onPinClick = function (evt) {
     }
 
     var currentIndex = mapAdPins.indexOf(target);
-    createCardHTML(ads[currentIndex]);
+    window.createCardHTML(ads[currentIndex]);
     mapFilter.before(mapCardTemplate);
     openAdCard();
   }
